b0690f0ce6b3c0ac65c48b5e3d25bafde3723eae
Orhan Emre <orhanemre.dev@gmail.com>
2022-04-22 23:29:27 +0300

    fix: motif info from json file

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <button class="z-[2] absolute md:top-4 bottom-4 right-4 w-12 h-12 bg-orange-400 rounded hover:bg-orange-300 transition" @click="toggleMenu">
         <img src="./assets/menu_FILL0_wght400_GRAD0_opsz48.svg" alt="menu">
     </button>
-    <div v-show="menu" class="absolute inset-0 z-[1] w-screen h-screen bg-slate-200 flex flex-col">
+    <div v-show="menu" class="p-10 gap-5 absolute inset-0 z-[1] w-screen h-screen bg-slate-100 flex flex-col">
         <button class="menu-item" @click="scan()">Motif Tara</button>
         <button class="menu-item" @click="toggleTeam()">Teknofest Takımı</button>
-        <a class="menu-item" href="https://github.com/orhanemree/argumareot">GitHub</a>
+        <a class="menu-item" href="https://github.com/orhanemree/Argumareot">GitHub</a>
     </div>
-    <div v-show="team" class="absolute inset-0 z-[1] w-screen h-screen bg-slate-200 flex flex-col">
+    <div v-show="team" class="absolute inset-0 z-[1] w-screen h-screen bg-slate-100 flex flex-col">
         <!-- TODO: Takım bilgilerini yaz -->
         Buraya takım ile ilgili bilgilendirmeler gelecek.
     </div>
--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,24 +1,27 @@
 <template>
-    <div>
+    <div class="flex items-center justify-center gap-12 flex-col md:flex-row">
         <div id="camera" class="relative">
             <button class="absolute bottom-[-1.5rem] left-0 right-0 z-[1] m-auto bg-black rounded-[50%] w-12 h-12 border-2 border-black" @click="capture()">
                 <img src="../assets/camera_FILL0_wght400_GRAD0_opsz48.svg" alt="camera">
             </button>
             <div class="z-[1] absolute top-3 left-3 right-0">
-                <span class="text-lg font-bold bg-slate-200 px-2">{{label}}</span>
+                <span v-show="motif.name" class="text-lg font-bold bg-slate-200 px-2">{{motif.name}}</span>
                 <!-- <span>{{confidence}}</span> -->
             </div>
+        </div>
+        <div class="md:w-64 w-[80%] mx-auto text-justify text-md">
+            {{motif.info}}
         </div>
     </div>
 </template>
 
 <script>
+import data from "../assets/data.json";
 export default {
     name: "Camera",
     data(){
         return{
-            label: "",
-            confidence: ""
+            motif: {}
         }
     },
     methods: {
@@ -32,12 +35,10 @@ export default {
         const classifyVideo = () => {
             classifier.classify(videoInput, (err, result) => {
                 if (err) throw err;
-                if (result[0].confidence * 100 > 95){    
-                    this.label = result[0].label;
-                    this.confidence = `%${Math.round(result[0].confidence * 100)}`;
+                if (result[0].confidence * 100 > 95){
+                    this.motif = data.filter(m => m.name === result[0].label)[0];
                 } else {
-                    this.label = "";
-                    this.confidence = "";
+                    this.motif = {}
                 }
                 classifyVideo();
             });
@@ -60,6 +61,7 @@ export default {
                 canvas.parent("camera");
 
                 videoInput = p.createCapture(p.VIDEO);
+                console.log(videoInput.width, videoInput.height);
                 videoInput.size(outputWidth, outputHeight);
                 videoInput.hide();
 
