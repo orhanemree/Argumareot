7d762769a123519fc275cb05c451d1c4e3717ab0
Orhan Emre <orhanemre.dev@gmail.com>
2022-05-13 22:14:14 +0300

    ready for teknofest!

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -2,16 +2,16 @@
     <div class="flex items-center justify-center gap-12 flex-col md:flex-row">
         <div id="camera" class="relative">
             <div class="z-[1] absolute top-3 left-3">
-                <span v-show="motif.name" class="text-lg font-bold bg-[#f6f8fa] px-2">{{motif.name}}</span>
+                <span v-show="motif.name" class="text-lg font-bold bg-[#f6f8fa] px-2">{{motif.title}}</span>
             </div>
             <div class="z-[1] absolute top-3 right-3 flex items-center justify-center gap-3">
-                <select @change="changeVideoSource($event)" class="bg-[#f6f8fa] rounded" name="devices">
+                <select @change="changeVideoSource($event)" class="bg-[#f6f8fa] rounded sm:text-base text-xs" name="devices">
                     <option>Kamerayı Değiştir</option>
                     <option v-for="device in devices" :key="device" :value="device.deviceId">{{device.label}}</option>
                 </select>
                 <button @click="mirrorVideo()" class="bg-[#f6f8fa] rounded px-2">
-                    <span>Aynala</span>
-                    <img class="h-6 inline" src="../assets/autorenew_FILL0_wght400_GRAD0_opsz48.svg" alt="mirror video">
+                    <span class="sm:text-base text-xs">Aynala</span>
+                    <img class="sm:h-6 h-2 inline" src="../assets/autorenew_FILL0_wght400_GRAD0_opsz48.svg" alt="mirror video">
                 </button>
             </div>
         </div>
@@ -67,7 +67,7 @@ export default {
         const classifyVideo = () => {
             classifier.classify(videoInput, (err, result) => {
                 if (err) throw err;
-                if (result[0].confidence * 100 > 95){
+                if (result[0].confidence * 100 > 95 && result[0].label !== "light" && result[0].label !== "dark"){
                     this.motif = data.filter(m => m.name === result[0].label)[0];
                 } else {
                     this.motif = {}
@@ -79,7 +79,7 @@ export default {
         // p5 script
         const script = p => {
             p.preload = _ => {
-                const modelUrl = "https://teachablemachine.withgoogle.com/models/7Jmpuujsv/model.json";
+                const modelUrl = "https://teachablemachine.withgoogle.com/models/EKbsPs3Cl/model.json";
                 classifier = ml5.imageClassifier(modelUrl);
 
                 // video ayalarını al (video kaynağı ve aynalama)
